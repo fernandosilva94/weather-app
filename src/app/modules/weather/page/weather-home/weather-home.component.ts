a5619c171e427c4dd77e7dbced27c770f3cdaa80
Fernando Silva <fernandoadmti@hotmail.com>
2024-12-16 17:38:06 -0300

    consumindo informacoes de cidades a partir da api

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service'; 
+import { WeatherDatas } from 'src/app/models/interfaces/WeatheDatas';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
   styleUrls: []
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit {
+  initialCityName = "Recife";
+  weatherDatas!: WeatherDatas;
+
+  constructor(private weatherService: WeatherService) {}
+  
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCityName);
+  }
+
+  getWeatherDatas(cityName: string): void {
+    this.weatherService.getWeatherDatas(cityName).subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response)
+        console.log("dados da city: ", this.weatherDatas)
+      },
+      error: (error) => console.log(error)
+    });
+  };
 
 }
