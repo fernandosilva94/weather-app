d64165db6f1835c498187e73d198b3eddc9f19b5
Fernando Silva <fernandoadmti@hotmail.com>
2024-12-18 11:29:52 -0300

    criacao do weather-card

---
+++ src/app/modules/weather/components/weather-card/weather-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { WeatherDatas } from 'src/app/models/interfaces/WeatheDatas';
+
+@Component({
+  selector: 'app-weather-card',
+  templateUrl: './weather-card.component.html',
+  styleUrls: []
+})
+export class WeatherCardComponent implements OnInit {
+  @Input() weatherDatasInput!: WeatherDatas;
+
+  ngOnInit(): void {
+    console.log("Dados recebidos do component pai(weather card): ", this.weatherDatasInput)
+  }
+}
